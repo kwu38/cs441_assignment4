bc51f329dcf251de4b7851c11a02f6360a27238b
Kevin Wu <kevinwu@Kevins-MacBook-Air-4.local>
2019-03-09 16:48:26 -0500

    Cards + Button

--- cs441_assignment4/app/src/main/java/com/example/threecardpoker/MainActivity.java
+++ cs441_assignment4/app/src/main/java/com/example/threecardpoker/MainActivity.java
@@ -11,21 +11,12 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    android.widget.Button deal_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        
     }
 
     @Override
